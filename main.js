6a4905f02d276dc281c0ee00811220708402827c
Squirrelcoding <rosilescarlos28@gmail.com>
2025-12-26 17:45:18 -0600

    added torus

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-import * as THREE from "three";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+// Scene and camera
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75, 
+  window.innerWidth / window.innerHeight, 
+  0.1, 
+  1000
+);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const target = new THREE.Vector3(0, 0, 0);
+controls.target.copy(target);
+
+controls.enablePan = true;
+controls.enableZoom = true;
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
+camera.position.set(5, 5, 5);
+controls.update();
+
+const geometry = new THREE.TorusGeometry();
+const material = new THREE.MeshNormalMaterial();
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
