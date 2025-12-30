b7748631f37b9c0ce54bee62ba10898189dfe5a2
Squirrelcoding <rosilescarlos28@gmail.com>
2025-12-30 01:46:10 -0600

    loaded mobius strip

--- app/components/Mobius.tsx
+++ app/components/Mobius.tsx
@@ -1,14 +1,18 @@
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js'
 import { useLoader } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 
 export default function Scene({ circlePos }) {
-	const obj = useLoader(OBJLoader, './mobius_2.obj');
-
+	const materials = useLoader(MTLLoader, "./mobius.mtl");
+	const obj = useLoader(OBJLoader, "./mobius.obj", (loader) => {
+		materials.preload();
+		loader.setMaterials(materials);
+	});
+	console.log(obj);
 	return (
 		<>
-			<primitive object={obj} />
-
+			<primitive object={obj} scale={0.4} />
 			<OrbitControls
 				target={[0, 0, 0]}
 				enablePan={true}
