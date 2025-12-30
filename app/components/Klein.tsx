aafe6c3bfe9e428972fd6dff1461e9c70a4d7590
Squirrelcoding <rosilescarlos28@gmail.com>
2025-12-30 02:23:45 -0600

    added klein bottle and mobius strip

--- app/components/Klein.tsx
+++ app/components/Klein.tsx
@@ -1,42 +1,51 @@
-import React, { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import * as THREE from 'three';
+import { MeshBasicMaterial, Vector3 } from "three";
+import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js';
+import { klein } from "three/addons/geometries/ParametricFunctions.js";
+import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
-function torusPoint(R: number, r: number, u: number, v: number) {
-	const uRad = u * 2 * Math.PI;
-	const vRad = v * 2 * Math.PI;
-	return new THREE.Vector3(
-		(R + r * Math.cos(vRad)) * Math.cos(uRad),
-		(R + r * Math.cos(vRad)) * Math.sin(uRad),
-		r * Math.sin(vRad)
-	);
+// Literally the same as the mobius3d but scaled
+function klein3dParameter(v: number, u: number) {
+	u *= Math.PI;
+	v *= 2 * Math.PI;
+
+	u = u * 2;
+	let x, z;
+	if (u < Math.PI) {
+
+		x = 3 * Math.cos(u) * (1 + Math.sin(u)) + (2 * (1 - Math.cos(u) / 2)) * Math.cos(u) * Math.cos(v);
+		z = - 8 * Math.sin(u) - 2 * (1 - Math.cos(u) / 2) * Math.sin(u) * Math.cos(v);
+
+	} else {
+
+		x = 3 * Math.cos(u) * (1 + Math.sin(u)) + (2 * (1 - Math.cos(u) / 2)) * Math.cos(v + Math.PI);
+		z = - 8 * Math.sin(u);
+
+	}
+
+	const y = - 2 * (1 - Math.cos(u) / 2) * Math.sin(v);
+
+	return new Vector3(x * 0.5, y * 0.5, z * 0.5);
 }
 
 export default function Scene({ circlePos }) {
+	const geometry = new ParametricGeometry(klein, 32, 32);
+	const material = new MeshBasicMaterial({ color: 0x00ff00 });
+
 	const pointRef = useRef();
-	const torusRadius = 1;
-	const torusTubeRadius = 0.4;
 
 	useFrame(() => {
 		if (pointRef.current) {
-			const position = torusPoint(torusRadius, torusTubeRadius, circlePos.u, circlePos.v);
+			const position = klein3dParameter(circlePos.u, circlePos.v);
 			pointRef.current.position.copy(position);
 		}
 	});
 
+
 	return (
 		<>
-			<mesh>
-				<torusGeometry args={[torusRadius, torusTubeRadius]} />
-				<meshNormalMaterial />
-			</mesh>
-
-			<mesh ref={pointRef}>
-				<sphereGeometry args={[0.05]} />
-				<meshBasicMaterial color={0xff0000} />
-			</mesh>
-
+			<mesh geometry={geometry} material={material} scale={0.5} />
 			<OrbitControls
 				target={[0, 0, 0]}
 				enablePan={true}
@@ -44,6 +53,11 @@ export default function Scene({ circlePos }) {
 				enableDamping={true}
 				dampingFactor={0.05}
 			/>
+
+			<mesh ref={pointRef}>
+				<sphereGeometry args={[0.05]} />
+				<meshBasicMaterial color={0xff0000} />
+			</mesh>
 		</>
 	);
 }
--- app/components/Mobius.tsx
+++ app/components/Mobius.tsx
@@ -1,14 +1,44 @@
-import { MeshBasicMaterial } from "three";
+import { MeshBasicMaterial, Vector3 } from "three";
 import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js';
 import { mobius3d } from "three/addons/geometries/ParametricFunctions.js";
 import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+// Literally the same as the mobius3d but scaled
+function mobius3dParameter(u: number, t: number) {
+	u *= Math.PI;
+	t *= 2 * Math.PI;
+
+	u = u * 2;
+	const phi = u / 2;
+	const major = 2.25, a = 0.125, b = 0.65;
+
+	let x = a * Math.cos( t ) * Math.cos( phi ) - b * Math.sin( t ) * Math.sin( phi );
+	const z = a * Math.cos( t ) * Math.sin( phi ) + b * Math.sin( t ) * Math.cos( phi );
+	const y = ( major + x ) * Math.sin( u );
+	x = ( major + x ) * Math.cos( u );
+
+	return new Vector3(x * 0.5, y * 0.5, z * 0.5);
+}
 
 export default function Scene({ circlePos }) {
-	const geometry = new ParametricGeometry(mobius3d, 20, 20);
+	const geometry = new ParametricGeometry(mobius3d, 32, 32);
 	const material = new MeshBasicMaterial({ color: 0x00ff00 });
+
+	const pointRef = useRef();
+
+	useFrame(() => {
+		if (pointRef.current) {
+			const position = mobius3dParameter(circlePos.u, circlePos.v);
+			pointRef.current.position.copy(position);
+		}
+	});
+
+
 	return (
 		<>
-			<mesh geometry={geometry} material={material} />
+			<mesh geometry={geometry} material={material} scale={0.5} />
 			<OrbitControls
 				target={[0, 0, 0]}
 				enablePan={true}
@@ -16,6 +46,11 @@ export default function Scene({ circlePos }) {
 				enableDamping={true}
 				dampingFactor={0.05}
 			/>
+
+			<mesh ref={pointRef}>
+				<sphereGeometry args={[0.05]} />
+				<meshBasicMaterial color={0xff0000} />
+			</mesh>
 		</>
 	);
 }
